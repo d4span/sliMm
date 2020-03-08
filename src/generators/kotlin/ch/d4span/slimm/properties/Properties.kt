811d45134534cc0e04b135a740c9f5d1e19d1a7b
David Caspar <david.caspar@aol.ch>
2020-03-08 15:05:05 +0100

    Added a few additional functions for dealing with properties.

--- src/generators/kotlin/ch/d4span/slimm/properties/Properties.kt
+++ src/generators/kotlin/ch/d4span/slimm/properties/Properties.kt
@@ -5,9 +5,14 @@ import kotlin.random.Random
 typealias Seed = Int
 typealias Outcome = Result<Unit>
 typealias Generator<T> = (Random) -> T
+
+fun <S, T> Generator<S>.map(f: (S) -> T): Generator<T> = { r: Random -> f(this(r)) }
+fun <S, T> Generator<S>.pair(gen: Generator<T>): Generator<Pair<S, T>> =
+        { r: Random -> this(r) to gen(r) }
+
 typealias Property<T> = (T) -> Outcome
 
-fun <T> forAll(gen: Generator<T>, seed: Seed = 100): Pair<Seed, Sequence<T>> {
+fun <T : Any> forAll(gen: Generator<T>, seed: Seed = 1): Pair<Seed, Sequence<T>> {
     val random = Random(seed)
     return seed to generateSequence { gen(random) }
 }
@@ -18,5 +23,5 @@ fun <T> Pair<Seed, Sequence<T>>.check(prop: Property<T>, size: Int = 100): Pair<
 fun <T> Pair<Seed, Sequence<Pair<T, Outcome>>>.verify(): Boolean =
         this.second.filter { it.second.isFailure }.none()
 
-fun <T> Pair<Seed, Sequence<Pair<T, Outcome>>>.failures() =
+fun <T> Pair<Seed, Sequence<Pair<T, Outcome>>>.failures(): Pair<Seed, Sequence<Pair<T, Outcome>>> =
         this.first to this.second.filter { it.second.isFailure }
